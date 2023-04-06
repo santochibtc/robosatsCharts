f39aed4edb905442b2284df39d0365e9f63d6172
santochibtc <santochibtc@protonmail.com>
2023-04-06 08:28:47 +0200

    add contractsPerMonth and volumePerMonth bar plots

--- generateCharts.py
+++ generateCharts.py
@@ -20,9 +20,7 @@ def generateCharts(api_url, proxy=()):
     groupedPerDay = df.groupby([pd.Grouper(key='timestamp', freq='D')]).agg({'count': 'sum', 'volume': 'sum'})
 
     sns.set_style("whitegrid")
-
-    #for each month and year accumulate the volume
-    #groupedPerMonth = df.groupby([pd.Grouper(key='timestamp', freq='M')]).agg({'premium': 'count', 'volume': 'sum'})
+   
     generateLineplot(groupedPerDay, "Contracts per day", "Date", "Number", "count", 1.5, "contractsPerDay.jpg")
     generateLineplot(groupedPerDay, "Traded volume per day (BTC)", "Date", "BTC", "volume", 'auto', "volumePerDay.jpg")
     
@@ -34,7 +32,28 @@ def generateCharts(api_url, proxy=()):
     groupedPerDay["count"] = groupedPerDay["count"].cumsum()
     generateLineplot(groupedPerDay, "Cumulative num contracts", "Date", "BTC", "count", 'auto', "cumulativeContracts.jpg")
 
-def generateLineplot(groupedPerDay, title, xlabel, ylabel, yfield, aspect, filename):
+    groupedPerMonth = df.groupby([pd.Grouper(key='timestamp', freq='M')]).agg({'count': 'count', 'volume': 'sum'})    
+    #replace timestamp with month name
+    groupedPerMonth.index = groupedPerMonth.index.strftime('%B')
+    generateBarplot(groupedPerMonth.sort_values(by=['count'], ascending=False), "Contracts per month", "Month", "Number", "count", 1.5, "contractsPerMonth.jpg")
+    generateBarplot(groupedPerMonth.sort_values(by=['volume'], ascending=False), "Volume per month", "Month", "Volume", "volume", 1.5, "volumePerMonth.jpg")
+
+
+def generateBarplot(data, title, xlabel, ylabel, yfield, aspect, filename):
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.xticks(rotation=90, fontsize=8)
+    ax = sns.barplot(data=data, x=data.index, y=yfield)
+    plt.gca().set_aspect(aspect)
+    ratio = 0.5
+    xleft, xright = ax.get_xlim()
+    ybottom, ytop = ax.get_ylim()
+    ax.set_aspect(abs((xright-xleft)/(ybottom-ytop))*ratio)
+    plt.savefig(filename, dpi=250, bbox_inches='tight')
+    plt.clf()
+
+def generateLineplot(data, title, xlabel, ylabel, yfield, aspect, filename):
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
